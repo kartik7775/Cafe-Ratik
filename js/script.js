7a2d34e5fdc1710816af5f0bdd1c8b66551b7478
Kartik Nikam <krt1993@outlook.com>
2023-07-12 04:10:16 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+import check from "../assets/check.svg";
+import star from "../assets/star.svg";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+
+
+
+
+
